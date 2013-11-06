556c81726db7977b0ad1b0d0130b421a7d1c7a0c
Corey Nagel <corey.nagel@colorado.edu>
2013-11-06 15:50:41 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoftwareMethodsProject::Application.config.secret_token = 'cb8edc3b2dc1a2ba5891066a9be1580a84b8686ed468dfb285ed0122dc6e21e5f72fddfec122707066936803a2ba842f7de0713bd39e50544c92b54742e6c043'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SoftwareMethodsProject::Application.config.session_store :cookie_store, key: '_software_methods_project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SoftwareMethodsProject::Application.config.session_store :active_record_store
